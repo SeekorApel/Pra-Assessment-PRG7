14a61ba4873b842373f1d460ed4be7ea32a18d8d
Dicky Muzakki <dickymuzakki05@gmail.com>
2024-07-02 17:13:50 +0700

    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,6 @@ import Navbar from "./component/layout";
 import Home from "./page/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./page/NotFound";
-import UserIndex from "./page/user/UserIndex";
-import UserCreate from "./page/user/UserCreate";
-import UserUpdate from "./page/user/UserUpdate";
-import ProductIndex from "./page/product/ProductIndex";
-import JenisIndex from "./page/jenis_product/JenisIndex";
 import LayananIndex from "./page/layanan/LayananIndex";
 import LayananCreate from "./page/layanan/LayananCreate";
 import LayananUpdate from "./page/layanan/LayananUpdate";
@@ -23,11 +18,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/user" element={<UserIndex />} />
-        <Route path="/user/create" element={<UserCreate />} />
-        <Route path="/user/update/:idUser" element={<UserUpdate />} />
-        <Route path="/product" element={<ProductIndex />} />
-        <Route path="/jenisproduct" element={<JenisIndex />} />
         <Route path="/layanan" element={<LayananIndex />} />
         <Route path="/layanan/create" element={<LayananCreate />} />
         <Route path="/layanan/update/:idLayanan" element={<LayananUpdate />} />
